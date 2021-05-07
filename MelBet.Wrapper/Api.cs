8bc5494caba27d75ef7035d3a51b8cb07b678474
ProgWidness <46569007+ProgWidness@users.noreply.github.com>
2021-05-07 17:34:16 -0400

    Add project files.

---
+++ MelBet.Wrapper/Api.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+
+namespace RoulletteParser.MelBet.Wrapper
+{
+    public class Api
+    {
+        private HttpClient _httpClient;
+
+        public Api()
+        {
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            _httpClient = new HttpClient(httpClientHandler);
+
+
+        }
+    }
+}
---
+++ RouletteParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RouletteParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
