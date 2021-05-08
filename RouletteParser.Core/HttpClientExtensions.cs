9e5bd4876cc258f3147bd0e3d408d3e3ec739b0d
ProgWidness <46569007+ProgWidness@users.noreply.github.com>
2021-05-08 15:23:57 -0400

    Realese two methods for configure http client

---
+++ RouletteParser.Core/HttpClientExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using RandomUserAgent;
+
+namespace RouletteParser.Core
+{
+    public static class HttpClientExtensions
+    {
+        private const string NameUserAgentHeader = "user-agent";
+        public static HttpClientHandler ConfigureHandler(this HttpClientHandler handler)
+        {
+            handler.UseCookies = true;
+            handler.CookieContainer = new CookieContainer();
+            handler.AllowAutoRedirect = true;
+            return handler;
+        }
+
+        public static HttpClient ConfigureClient(this HttpClient client)
+        {
+            client.DefaultRequestHeaders.Add(NameUserAgentHeader, RandomUa.RandomUserAgent);
+            return client;
+        }
+
+
+    }
+}
