dd8df2843fe3ea427bb3413bd18f59211e79049d
ProgWidness <46569007+ProgWidness@users.noreply.github.com>
2021-05-08 08:48:30 -0400

    Get nuget for work with api rucapthca

---
+++ RouletteParser.RuCaptcha/Api.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouletteParser.RuCaptcha
+{
+    public class Api
+    {
+        private TwoCaptcha.TwoCaptcha _solver;
+
+        public Api(string token)
+        {
+            TwoCaptcha.TwoCaptcha solver = new TwoCaptcha.TwoCaptcha(token);
+        }
+
+        public async Task<double> GetBalance()
+        {
+            return await _solver.Balance();
+        }
+    }
+}
