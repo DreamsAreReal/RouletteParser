088738b8a0d3a9875cd070bb98d3b4c5a7c1f0f2
ProgWidness <46569007+ProgWidness@users.noreply.github.com>
2021-05-08 15:17:11 -0400

    Make constructor and new project for public methods

---
+++ RouletteParser.GrandCasino/Api.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RouletteParser.GrandCasino
+{
+    public class Api
+    {
+        public HttpClient Client => _client;
+
+        private HttpClient _client;
+
+        public Api()
+        {
+            _client = new HttpClient();
+
+
+        }
+    }
+}
